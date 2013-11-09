e713d4162128eb75883d75bf7c06d63c849b5735
tonyskn <tonyskn@gmail.com>
2013-11-09 09:40:17 +0100

    Week1: Quickcheck

--- w1-quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ w1-quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -14,7 +14,44 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = ???
+//  property("min2") = forAll { (h: H) =>
+//    val m = if (isEmpty(h)) 0 else findMin(h)
+//    findMin(insert(m,h)) == m
+//  }
+//
+//  property("inserting_2_els_into_empty_heap") = forAll { (a: A, b: A) =>
+//    val h = insert(a, insert(b, empty))
+//    val m = findMin(h)
+//    m == ord.min(a, b)
+//  }
+
+//  property("inserting_then_deleting_minimum") = forAll { (a: A) =>
+//    val h = insert(a, empty)
+//    isEmpty(deleteMin(h))
+//  }
+
+//  property("minimum_of_melding") = forAll { (h1: H, h2: H) =>
+//    val minMelded = findMin( meld(h1, h2) )
+//    minMelded == findMin(h1) || minMelded == findMin(h2)
+//  }
+
+//  property("asList_should_be_sorted") = forAll { (h: H) =>
+//    asList(h).sorted == asList(h) 
+//  }
+
+  property("inserting_sequence") = forAll { (l: List[A]) =>
+    var h = l.foldRight(empty)(insert)
+    asList(h) == l.sorted
+  }
+
+  def asList(h: H): List[A] = // the sorted list resulting from extracting all elements of h
+   if (isEmpty(h)) List() else findMin(h) :: asList(deleteMin(h))
+
+
+  lazy val genHeap: Gen[H] = for {
+    k <- arbitrary[A]
+    h <- oneOf(empty, genHeap)
+  } yield insert(k, h)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
